0ec3d580047140a99036a46d65996b3678545414
Mitchel <mitchel_jager@hotmail.com>
2022-08-21 18:37:52 +0200

    Mock ResizeObserver in tests

--- test/TinyGallery.test.js
+++ test/TinyGallery.test.js
@@ -2,6 +2,17 @@ import "jsdom-global/register"
 import { render } from "@testing-library/svelte"
 import TinySlider from '../src/TinySlider.svelte'
 
+beforeEach(() => {
+  Object.defineProperty(global, "ResizeObserver", {
+    writable: true,
+    value: jest.fn().mockImplementation(() => ({
+        observe: jest.fn(),
+        unobserve: jest.fn(),
+        disconnect: jest.fn(),
+    })),
+  })
+})
+
 test("Renders", () => {
   render(TinySlider)
 })
