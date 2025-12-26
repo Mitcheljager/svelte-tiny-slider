6c4ea94f3f528af0cba49f21231f3e5a849abbfb
Mitchel <mitchel_jager@hotmail.com>
2025-12-26 23:16:18 +0100

    docs: Add Tailwind example to docs

---
+++ src/routes/tailwind/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import TinySlider from "$lib/TinySlider.svelte";
+</script>
+
+<div>
+  <TinySlider>
+    {#each ["https://picsum.photos/seed/a/200/200", "https://picsum.photos/seed/b/200/200/", "https://picsum.photos/seed/c/200/200/"] as item}
+      <img src={item} alt="" class="max-w-screen" />
+    {/each}
+  </TinySlider>
+</div>
+
+<div class="mt-20">
+  <TinySlider>
+    {#each ["https://picsum.photos/seed/a/200/200", "https://picsum.photos/seed/b/200/200/", "https://picsum.photos/seed/c/200/200/"] as item}
+      <div><img src={item} alt="" /></div>
+    {/each}
+  </TinySlider>
+</div>
+
+<style>
+  @import "tailwindcss";
+
+  :global(:root) {
+    color-scheme: light dark;
+  }
+
+  :global(body) {
+    background: light-dark(white, black);
+  }
+
+  div {
+    width: 200px;
+  }
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => ({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), tailwindcss()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"]
 	},
